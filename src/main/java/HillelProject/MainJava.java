cddb6a386a9d5c7b7ead94112c638f80f836deb1
Stayakot <pentagram.secret@gmail.com>
2022-06-02 10:52:34 +0300

    HillelProject2

--- src/main/java/HillelProject/MainJava.java
+++ src/main/java/HillelProject/MainJava.java
@@ -10,7 +10,7 @@ public class MainJava {
     private static final String dbUrl = "jdbc:postgresql://ec2-54-75-184-144.eu-west-1.compute.amazonaws.com:5432/d58pld23fdkd1a";
     private static final String username = "psuzbqizgvtxiz";
     private static final String password = "c68e118fccbd4adfc9d6ebc9838595325368b65791256f9ff5197491cc813672";
- Connection connection;
+    Connection connection;
 
     private static Connection getConnection() throws URISyntaxException, SQLException {
         URI dbUri = new URI(System.getenv("DATABASE_URL"));
@@ -33,4 +33,3 @@ public class MainJava {
     }
 
 }
-
--- src/main/java/HillelProject/Test.java
+++ src/main/java/HillelProject/Test.java
@@ -1,39 +1,39 @@
-//package HillelProject;
-//
-//import java.sql.ResultSet;
-//import java.sql.SQLException;
-//import java.sql.Statement;
-//import java.util.Scanner;
-//
-//public class Test {
-//    public static void main(String[] args) {
-//
-//        Scanner scanner = new Scanner(System.in);
-//        String product = scanner.nextLine();
-//        MainJava mainJava = new MainJava();
-//        String sqlWorker = ("Select * from d58pld23fdkd1a.products.products");
-//
-//        try {
-//            Statement statement = mainJava.connection.createStatement();
-//            ResultSet resultSet = statement.executeQuery(sqlWorker);
-//            while (resultSet.next()) {
-//                Products products = new Products();
-//                String result = resultSet.getString(1);
-//                if (result.toUpperCase().contains(product.toUpperCase())) {
-//                    products.setId(resultSet.getInt(5));
-//                    products.setProduct_name(resultSet.getString(1));
-//                    products.setProtein(resultSet.getDouble(2));
-//                    products.setCarbohydrates(resultSet.getDouble(3));
-//                    products.setFats(resultSet.getDouble(4));
-//                    System.out.println(products);}
-//                else if(!resultSet.next() && !result.contains(product)){
-//                    System.out.println("Такого продукта нет");
-//
-//                }
-//            }
-//        } catch (SQLException e) {
-//            throw new RuntimeException(e);
-//        }
-//
-//    }
-//}
+package HillelProject;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String product = scanner.nextLine();
+        MainJava mainJava = new MainJava();
+        String sqlWorker = ("Select * from d58pld23fdkd1a.products.products");
+
+        try {
+            Statement statement = mainJava.connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sqlWorker);
+            while (resultSet.next()) {
+                Products products = new Products();
+                String result = resultSet.getString(1);
+                if (result.toUpperCase().contains(product.toUpperCase())) {
+                    products.setId(resultSet.getInt(5));
+                    products.setProduct_name(resultSet.getString(1));
+                    products.setProtein(resultSet.getDouble(2));
+                    products.setCarbohydrates(resultSet.getDouble(3));
+                    products.setFats(resultSet.getDouble(4));
+                    System.out.println(products);}
+                else if(!resultSet.next() && !result.contains(product)){
+                    System.out.println("Такого продукта нет");
+
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
