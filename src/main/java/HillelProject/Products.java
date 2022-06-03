e8955b509a8c6505fc5e74942e45a5f1a71eb7b3
Stayakot <pentagram.secret@gmail.com>
2022-06-03 12:53:04 +0300

    HillelProject2

--- src/main/java/HillelProject/Products.java
+++ src/main/java/HillelProject/Products.java
@@ -1,9 +1,23 @@
 package HillelProject;
 
+import java.util.Objects;
+
 /* Класс Products, создан для переменных из Таблицы Базы Данных, в случае, если продукт будет в базе
 то переменные берут значение этих продуктов.
 */
 public class Products {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Products)) return false;
+        Products products = (Products) o;
+        return Double.compare(products.calories, calories) == 0 && Double.compare(products.protein, protein) == 0 && Double.compare(products.carbohydrates, carbohydrates) == 0 && Double.compare(products.fats, fats) == 0 && product_name.equals(products.product_name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(calories, product_name, protein, carbohydrates, fats);
+    }
 
     private double calories;
     private String product_name;
