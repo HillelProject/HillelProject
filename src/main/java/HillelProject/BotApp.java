4e3854b4971707b0783a38bf5b18664d0f8539d2
Stayakot <pentagram.secret@gmail.com>
2022-06-04 16:30:06 +0300

    HillelProject2

--- src/main/java/HillelProject/BotApp.java
+++ src/main/java/HillelProject/BotApp.java
@@ -44,7 +44,7 @@ public class BotApp {
 
         try {
             // комманда для SQL которая выводит базу данных
-            String sqlWorker = ("select * from d58pld23fdkd1a.products.products where \"Products_Name\" like " + "\'%" + message + "%\'");
+            String sqlWorker = ("select * from d58pld23fdkd1a.products.products where \"Products_Name\" ilike " + "\'%" + message + "%\'");
 
             // Создаем подключение к базе данных
             Statement statement = mainJava.connection.createStatement();
