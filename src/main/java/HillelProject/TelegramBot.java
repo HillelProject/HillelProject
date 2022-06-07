6213a0a41ea528ae0acaaef26dcbec1570517190
Stayakot <pentagram.secret@gmail.com>
2022-06-07 19:55:00 +0300

    HillelProject2

--- src/main/java/HillelProject/TelegramBot.java
+++ src/main/java/HillelProject/TelegramBot.java
@@ -1,16 +1,17 @@
-
 package HillelProject;
 
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.methods.updates.GetUpdates;
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,72 +38,112 @@ public class TelegramBot extends TelegramLongPollingBot {
             if (message.getText().equals("Калории продуктов")) {
                 String messages = update.getMessage().getText();
                 String response = BotApp.process(messages);
-                sendText(message, response);
+                inlineButton(message, "Введите продукт");
 
+            }} else if (update.hasCallbackQuery()) {
+                String st1= String.valueOf(update.getCallbackQuery().getMessage().getChatId());
+                CallbackQuery callbackQuery = new CallbackQuery();
+                String data = callbackQuery.getData();
+                SendMessage sendMessage = new SendMessage();
+                sendMessage.setChatId(st1);
+                if (data.equals("Сancel")) {
+                    sendMessage.setText("Окей");
+                } else {
+                    sendMessage.setText(BotApp.process("Рецепт Дня"));
+                }
+                try {
+                   execute(sendMessage);
+                } catch (RuntimeException e) {
+
+                } catch (TelegramApiException e) {
+                    throw new RuntimeException(e);
+                }
+
+        }
+        }
+
+
+        private void sendText (Message message, String text){
+            SendMessage sendMessage = new SendMessage();
+            sendMessage.enableMarkdown(true);
+
+            // Создаем клавиатуру
+            ReplyKeyboardMarkup replyKeyboardMarkup = new
+                    ReplyKeyboardMarkup();
+            sendMessage.setReplyMarkup(replyKeyboardMarkup);
+            replyKeyboardMarkup.setSelective(true);
+            replyKeyboardMarkup.setResizeKeyboard(true);
+            replyKeyboardMarkup.setOneTimeKeyboard(false);
+
+            // Создаем список строк клавиатуры
+            List<KeyboardRow> keyboard = new ArrayList<>();
+
+            // Первая строчка клавиатуры
+            KeyboardRow keyboardFirstRow = new KeyboardRow();
+            // Добавляем кнопки в первую строчку клавиатуры
+            keyboardFirstRow.add("Рецепт Дня");
+            keyboardFirstRow.add("Индивидуальный счетчик калорий");
+
+            // Вторая строчка клавиатуры
+            KeyboardRow keyboardSecondRow = new KeyboardRow();
+            // Добавляем кнопки во вторую строчку клавиатуры
+            keyboardSecondRow.add("Калории продуктов");
+            keyboardSecondRow.add("Анекдот Дня");
+
+            // Добавляем все строчки клавиатуры в список
+            keyboard.add(keyboardFirstRow);
+            keyboard.add(keyboardSecondRow);
+            // и устанавливаем этот список нашей клавиатуре
+            replyKeyboardMarkup.setKeyboard(keyboard);
+            sendMessage.setChatId(message.getChatId().toString());
+            sendMessage.setReplyToMessageId(message.getMessageId());
+            sendMessage.setText(text);
+            try {
+                execute(sendMessage);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
             }
-
-
-
-
-
         }
-    }
-
-    private void sendText(Message message, String text) {
-        SendMessage sendMessage = new SendMessage();
-        sendMessage.enableMarkdown(true);
 
 
-        // Создаем клавиатуру
-        ReplyKeyboardMarkup replyKeyboardMarkup = new
-                ReplyKeyboardMarkup();
-        sendMessage.setReplyMarkup(replyKeyboardMarkup);
-        replyKeyboardMarkup.setSelective(true);
-        replyKeyboardMarkup.setResizeKeyboard(true);
-        replyKeyboardMarkup.setOneTimeKeyboard(false);
+        private void inlineButton (Message message, String text){
+            SendMessage sendMessage = new SendMessage();
+            sendMessage.enableMarkdown(true);
+            //Inline KeyBoard
+            InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
 
-        // Создаем список строк клавиатуры
-        List<KeyboardRow> keyboard = new ArrayList<>();
+            List<List<InlineKeyboardButton>> inlineButtons = new ArrayList<>();
+            List<InlineKeyboardButton> inlineKeyboardButtons = new ArrayList<>();
+            InlineKeyboardButton inlineKeyboardButton = new InlineKeyboardButton();
+            inlineKeyboardButton.setText("Cancel");
+            inlineKeyboardButton.setCallbackData("Cancel");
+            inlineKeyboardButtons.add(inlineKeyboardButton);
+            inlineButtons.add(inlineKeyboardButtons);
+            inlineKeyboardMarkup.setKeyboard(inlineButtons);
 
-        // Первая строчка клавиатуры
-        KeyboardRow keyboardFirstRow = new KeyboardRow();
-        // Добавляем кнопки в первую строчку клавиатуры
-        keyboardFirstRow.add("Рецепт Дня");
-        keyboardFirstRow.add("Индивидуальный счетчик калорий");
-
-        // Вторая строчка клавиатуры
-        KeyboardRow keyboardSecondRow = new KeyboardRow();
-        // Добавляем кнопки во вторую строчку клавиатуры
-        keyboardSecondRow.add("Калории продуктов");
-        keyboardSecondRow.add("Анекдот Дня");
-
-        // Добавляем все строчки клавиатуры в список
-        keyboard.add(keyboardFirstRow);
-        keyboard.add(keyboardSecondRow);
-        // и устанавливаем этот список нашей клавиатуре
-        replyKeyboardMarkup.setKeyboard(keyboard);
+            sendMessage.setReplyMarkup(inlineKeyboardMarkup);
 
 
-        sendMessage.setChatId(message.getChatId().toString());
-        sendMessage.setReplyToMessageId(message.getMessageId());
-        sendMessage.setText(text);
-        try {
-            execute(sendMessage);
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
+            sendMessage.setChatId(message.getChatId().toString());
+            sendMessage.setReplyToMessageId(message.getMessageId());
+            sendMessage.setText(text);
+            try {
+                execute(sendMessage);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
         }
+
+
+        @Override
+        public String getBotUsername () {
+            return "Calories_Products_Bot";
+        }
+
+        @Override
+        public String getBotToken () {
+            return "5415629103:AAHIS7AIxqOOU5bAc58nk1I9w-aqCuyYDG0";
+        }
+
+
     }
-
-
-    @Override
-    public String getBotUsername() {
-        return "Calories_Products_Bot";
-    }
-
-    @Override
-    public String getBotToken() {
-        return "5415629103:AAHIS7AIxqOOU5bAc58nk1I9w-aqCuyYDG0";
-    }
-
-
-}
