5d424f3a0e732c376593257549a1c0c746c1ba92
Best) <pentagram.secret@gmail.com>
2022-06-08 23:02:21 +0300

    update IndividualData

--- src/main/java/HillelProject/IndividualData.java
+++ src/main/java/HillelProject/IndividualData.java
@@ -3,18 +3,19 @@ package HillelProject;
 import java.util.HashMap;
 
 public class IndividualData {
-    private static double womanHeight = 3.1;
-    private static double manHeight = 4.8;
-    private static double manWeight = 13.4;
-    private static double womanWeight = 9.2;
-    private static double manYear = 5.7;
-    private static double womanYear = 4.3;
+
     //private double calorieExpenditurePerDay;  //трата калорий в день
 
 
-    static HashMap<String, String> hashForIndividualCaloriesCalculation = new HashMap<>();
+
 
     public static String individualCaloriesCalculation(HashMap<String, String> hashMap) {
+         double womanHeight = 3.1;
+          double manHeight = 4.8;
+          double manWeight = 13.4;
+          double womanWeight = 9.2;
+          double manYear = 5.7;
+          double womanYear = 4.3;
         double result=1;
         if (Double.parseDouble(hashMap.get("5"))==88.36) {
             result =Double.parseDouble(hashMap.get("6"))*( Double.parseDouble(hashMap.get("5")) + (manWeight * Double.parseDouble(hashMap.get("3")))+(manHeight*Double.parseDouble(hashMap.get("2")))
