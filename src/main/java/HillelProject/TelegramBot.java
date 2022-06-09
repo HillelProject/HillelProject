cb662e8019b8c5dd06068f0cfa63596a1cb60ec9
Stayakot <pentagram.secret@gmail.com>
2022-06-09 19:17:42 +0300

    HillelProject2

--- src/main/java/HillelProject/TelegramBot.java
+++ src/main/java/HillelProject/TelegramBot.java
@@ -120,11 +120,13 @@ public class TelegramBot extends TelegramLongPollingBot {
             if (data.equals("Хватит")){
                 sendMessage.setChatId(String.valueOf(message1.getChatId()));
                 sendMessage.setText("Хорошо");
+                hashForProducts.clear();
 
             }
             if (data.equals("Cancel")){
                 sendMessage.setChatId(String.valueOf(message1.getChatId()));
                 sendMessage.setText("Хорошо");
+                hashForProducts.clear();
 
             }
 
