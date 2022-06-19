3cb36b8cdd309a097e47ad5823e5f63187cd5707
VadPol95 <vpolyvyanoy@gmail.com>
2022-06-19 11:37:24 +0300

    add

--- src/main/java/HillelProject/TelegramBot/TelegramBot.java
+++ src/main/java/HillelProject/TelegramBot/TelegramBot.java
@@ -255,7 +255,7 @@ public class TelegramBot extends TelegramLongPollingBot {
                 // Реакция на выбор "Нагрузки 6–7 раз в неделю" в Inline меню
             } else if (data.equals("Нагрузки 6–7 раз в неделю") && IndividualDataMethods.checkIndividualCaloriesCalculation(String.valueOf(message1.getChatId()), "5")) {
                 InsertIndividualDataSQL.process(String.valueOf(message1.getChatId()), "6", "1.725");
-                sendMessage.setText("U+1F601" + "Ваша суточная норма употребления каллорий: " + IndividualDataMethods.individualCaloriesCalculation(String.valueOf(message1.getChatId())));
+                sendMessage.setText("Ваша суточная норма употребления каллорий: " + IndividualDataMethods.individualCaloriesCalculation(String.valueOf(message1.getChatId())));
                 IndividualDataMethods.deleteIndividualCaloriesCalculation(String.valueOf(message1.getChatId()));
 
                 // Реакция на выбор "Ежедневно более одной тренировки" в Inline меню
